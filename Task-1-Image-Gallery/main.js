f5815ef2d769e67935295094331eade052398a34
Ojo Farouq Abiodun <harbyodunojo@gmail.com>
2025-11-27 03:06:58 +0100

    task completed

--- Task-1-Image-Gallery/main.js
+++ Task-1-Image-Gallery/main.js
@@ -5,7 +5,7 @@ const closeBtn = document.querySelector(".close");
 
 let currentIndex = 0;
 
-// Open Lightbox
+
 galleryImages.forEach((img, index) => {
     img.addEventListener("click", () => {
         currentIndex = index;
@@ -16,7 +16,7 @@ setTimeout(() => lightbox.style.opacity = "1", 10);
     });
 });
 
-// Close Lightbox
+
 closeBtn.addEventListener("click", () => {
     lightbox.style.opacity = "0";
 setTimeout(() => {
@@ -25,7 +25,7 @@ setTimeout(() => {
 ;
 });
 
-// Navigate Images
+
 document.getElementById("nextBtn").addEventListener("click", () => {
     currentIndex = (currentIndex + 1) % galleryImages.length;
     showImage();
